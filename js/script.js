c775b7eb54d4acdbd099f24057dee60301a12d66
Kireeva <kireeva.en@defo.ru>
2021-12-02 12:38:40 +0300

    slider 1 - простой

--- js/script.js
+++ js/script.js
@@ -248,33 +248,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
             });
 
-
-            // Второй способ создания карточек, можем его использовать когда нам не нужна шаблонизация, а нужно создать только 1 элемент. Тогда экземпляр класса не нужен 
-    // getResource('http://localhost:3000/menu')
-    //     .then(data => createCard(data));
-
-    // function createCard(data) {
-    //     data.forEach(({img, altimg, title, descr, price}) => {
-    //         const element = document.createElement('div');
-
-    //         element.classList.add("menu__item");
-
-    //         element.innerHTML = `
-    //             <img src=${img} alt=${altimg}>
-    //             <h3 class="menu__item-subtitle">${title}</h3>
-    //             <div class="menu__item-descr">${descr}</div>
-    //             <div class="menu__item-divider"></div>
-    //             <div class="menu__item-price">
-    //                 <div class="menu__item-cost">Цена:</div>
-    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
-    //             </div>
-    //         `;
-    //         document.querySelector(".menu .container").append(element);
-    //     });
-    // }
-
     
-
 
     // Forms
 
@@ -360,6 +334,58 @@ window.addEventListener('DOMContentLoaded', () => {
         // fetch('http://localhost:3000/menu')  // обращаемся к нашей базе данных
         //     .then(data => data.json())   // превращаем ответ от сервера (json-файл) в обычный js-объект
         //     .then(res => console.log(res));
+
+
+
+        // Slider
+
+        const slides = document.querySelectorAll('.offer__slide'),
+              prev = document.querySelector('.offer__slider-prev'),
+              next = document.querySelector('.offer__slider-next'),
+              total = document.querySelector('#total'),
+              current = document.querySelector('#current');
+        let slideIndex = 1;
+
+        showSlides(slideIndex);
+
+        if (slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+        } else {
+            total.textContent = slides.length;
+        }
+
+        function showSlides(n) {
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+
+            slides.forEach(item => item.style.display = 'none'); // перебираем массив слайдов и прячем их все под display: none;
+
+            slides[slideIndex - 1].style.display = 'block'; // показываем первый слайд (с индексом 0)
+
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+        }
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n) // slideIndex будет увеличен на значение n
+        }
+
+        prev.addEventListener('click', () => {
+            plusSlides(-1);
+        });
+
+        next.addEventListener('click', () => {
+            plusSlides(1);
+        });
+
 
 
 
